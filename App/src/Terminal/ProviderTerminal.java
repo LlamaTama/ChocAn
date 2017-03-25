9f798fbe8be179fcb1420ac728c127c3812e45bd
Tanesh Manjrekar <tanesh.manjrekar@gmail.com>
2017-03-24 22:00:07 -0700

    Created Provider Terminal

---
+++ App/src/Terminal/ProviderTerminal.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2017 Tanesh Manjrekar
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package Terminal;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+/**
+ *
+ * @author Tanesh Manjrekar
+ */
+public class ProviderTerminal extends JFrame implements ActionListener
+{
+    private ProviderTerminal()
+    {
+        
+    }
+    
+    public static void createProviderTerminal()
+    {
+        ProviderTerminal pt = new ProviderTerminal();
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent ae)
+    {
+        
+    }
+}
