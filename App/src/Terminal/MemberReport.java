b4cee8a364b71a6c5718e19ddfebd538774d23b9
Tanesh Manjrekar <tanesh.manjrekar@gmail.com>
2017-03-29 16:13:41 -0700

    Fixed file name

--- App/src/Terminal/MemberReport.java
+++ App/src/Terminal/MemberReport.java
@@ -42,7 +42,7 @@ public class MemberReport
         id = memID;
         lastDate = lDate;
         currentDate = cDate;
-        filename = "Reports\\MemberReport" + id + lDate.toString() + " - " + cDate.toString() + ".xls";
+        filename = "Reports\\MemberReport " + id + ".xls";
         
         HSSFWorkbook hwb = new HSSFWorkbook();
         HSSFSheet sheet = hwb.createSheet("Weekly Report");
